1ee9e349bcaa2349585746a90dddf8c6ac98a514
prashanth0926 <prashanth.foodsurfing@gmail.com>
2022-05-30 22:54:12 -0500

    update env vars

--- src/Pages/AncesTree/axiosInstance.ts
+++ src/Pages/AncesTree/axiosInstance.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export default () => {
+  const API_URL = process.env.REACT_APP_API_URL;
   const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080/api/',
+    baseURL: API_URL || 'http://localhost:8080/api/',
     timeout: 30000,
     headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}`}
   });
